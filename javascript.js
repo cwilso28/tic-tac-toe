c77069e70830e6d1fa8466b73c7dccdeaedd2f0f
Chris <47066922+cwilso28@users.noreply.github.com>
2025-02-19 09:20:54 -0600

    Add function to check for matching symbols
    The function enables to program to check for matching symbols in an
    array. Future logic should be added to prevent an array with all nulls
    from getting passed to the function as this will pass the check but
    represents and empty row.

--- javascript.js
+++ javascript.js
@@ -13,6 +13,19 @@ function ticTacToeBoard() {
             board[coord[0]][coord[1]] = symbol;
         };
     };
+
+    function checkForMatch(array) {
+        const arrayIsTheSame = array.every(x => x === array[0])
+        return arrayIsTheSame;
+    }
+
+    function checkBoardState(board) {
+        // Isolate locations to check for wins
+        const boardDiag1 = [board[0][0], board[1][1], board[2][2]];
+        const boardDiag2 = [board[0][2], board[1][1], board[2][0]];
+        const boardArray=[board[0], board[1], board[2], board.map(x => x[0]), board.map(x=>x[1]), board.map(x=>x[2]), boardDiag1, boardDiag2];
+        
+    }
     return {addSymbol, board}
 };
 
