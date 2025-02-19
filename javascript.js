1300c717db10ecc1c7687c65faaf4557239327d2
Chris <47066922+cwilso28@users.noreply.github.com>
2025-02-18 21:42:49 -0600

    Add functions to create player and board
    The function to create player allows th user to input a name and
    choose a symbol to use during the game. The function to create the board
    generates and empty board and allows for modification of said board.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
-document.querySelector(".test").textContent="Hello!"
+function createPlayer(name, symbol) {
+    let score = 0;
+    const getScore = () => score;
+    const addToScore = () => score++;
+    return {name, symbol, getScore, addToScore}
+}
+
+function ticTacToeBoard() {
+    let board = [[null, null, null],[null, null, null],[null, null, null]];
+
+    function addSymbol(coord, symbol) {
+        board[coord[0]][coord[1]] = symbol;
+    }
+    return {addSymbol, board}
+}
+
+const tim = createPlayer("tim", "o");
+tim.addToScore();
+
+console.log({playerName: tim.name, score: tim.getScore()})
